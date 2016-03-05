85bf0d871a0a737bbd845b194ba5a76acbf3ecfb
Myk Melez <myk@mykzilla.org>
2016-03-04 17:17:42 -0800

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -1,220 +1,160 @@
-/**
- * Copyright 2015 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// This generated service worker JavaScript will precache your site's resources.
-// The code needs to be saved in a .js file at the top-level of your site, and registered
-// from your pages in order to be used. See
-// https://github.com/googlechrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
-// for an example of how you can register this script and handle various service worker events.
-
-/* eslint-env worker, serviceworker */
-/* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren */
-'use strict';
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 
+(function (self) {
+  'use strict';
 
-/* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["CNAME","cf9860517eff73bd87526dc776ece7f3"],["css/index.css","898c39bc9ad7657e9549f9849d254dd5"],["favicon/android-chrome-144x144.png","55977723c23cade5948e7ea54c30f432"],["favicon/android-chrome-192x192.png","2f8b44101cf93c8756b825623f6cadf7"],["favicon/android-chrome-36x36.png","505a3e5f8f349b37d99719eea0dcee82"],["favicon/android-chrome-48x48.png","566feb9f62b14475ed47149cfeac95bc"],["favicon/android-chrome-72x72.png","9a786e8ec2dd57cd1af92f95eb1d1c66"],["favicon/android-chrome-96x96.png","adc3451ed08c84190eb2886908a9d66b"],["favicon/apple-touch-icon-114x114.png","978153c34c7f052847af738fdae874be"],["favicon/apple-touch-icon-120x120.png","d8f2a95598f6b65ade43529443a913a7"],["favicon/apple-touch-icon-144x144.png","248b38cd0957489272e65de6e4995d08"],["favicon/apple-touch-icon-152x152.png","b048936694550bf8da1106d8f6b6c5f1"],["favicon/apple-touch-icon-180x180.png","ac44b197f399dd65dfba624124a991ac"],["favicon/apple-touch-icon-57x57.png","98108f51ea3697bc473341c2d522b9f7"],["favicon/apple-touch-icon-60x60.png","84685a7225287f94b3151a0109aa6ffc"],["favicon/apple-touch-icon-72x72.png","7b36f661628e63d235910d8dfd8115f4"],["favicon/apple-touch-icon-76x76.png","a081394f17687c0b05b765072243d25b"],["favicon/apple-touch-icon-precomposed.png","85ead418566f180da7aa8ee16fbbd91a"],["favicon/apple-touch-icon.png","ac44b197f399dd65dfba624124a991ac"],["favicon/browserconfig.xml","ecf8f21c7aaec3133a29a16a8f0bfb57"],["favicon/favicon-16x16.png","bf63fc73494410b80cec3031194ac618"],["favicon/favicon-194x194.png","735cf7061406c6c097d863649c3b7598"],["favicon/favicon-32x32.png","2a33a1f073f2d53c6f2ff2aae2fab1cb"],["favicon/favicon-96x96.png","d60a5795d5ea64ced6247d88a0bb135d"],["favicon/favicon.ico","003763b9beaeb64bc1e0adf6e1443c9b"],["favicon/manifest.json","45ca79381809c2b75b0307868481277e"],["favicon/mstile-144x144.png","fdf45a44d2f2a28497a9afd531d99bff"],["favicon/mstile-150x150.png","584b9dcadcc9da11464a9a1550754435"],["favicon/mstile-310x150.png","0368a47941d17ec1157bae5e199212cb"],["favicon/mstile-310x310.png","d307e42284c46931eafa51d8f7ca171b"],["favicon/mstile-70x70.png","345e6ef2fe7ce3ceab8dec66477ae757"],["img/pause.svg","130f551805b60be423ce23ed6f46625d"],["img/reset.svg","8728572dea4cea5dcbcc8e057b5f04bd"],["img/start.svg","5d4ee3505749db2006f676cf2a5624ce"],["index.css","3ce44e48f8d25e8a10dc1babfeb57f11"],["index.html","5960e5f043322d59c65bd6f935510efe"],["index.js","2a4f40a8d7cefb3d321bed3507b08df1"],["js/index.js","156dc22d65a06e70137baa8c24277864"],["js/offline-manager.js","28bd78ca4fcf8ae2b562c51a1cd2f5d3"],["scripts/offline-manager.js","28bd78ca4fcf8ae2b562c51a1cd2f5d3"]];
-/* eslint-enable quotes, comma-spacing */
-var CacheNamePrefix = 'sw-precache-v1-mykmelez/eggtimer-' + (self.registration ? self.registration.scope : '') + '-';
-
-
-var IgnoreUrlParametersMatching = [/./];
-
-
-
-var addDirectoryIndex = function (originalUrl, index) {
-    var url = new URL(originalUrl);
-    if (url.pathname.slice(-1) === '/') {
-      url.pathname += index;
-    }
-    return url.toString();
-  };
-
-var populateCurrentCacheNames = function (precacheConfig, cacheNamePrefix, baseUrl) {
-    var absoluteUrlToCacheName = {};
-    var currentCacheNamesToAbsoluteUrl = {};
-
-    precacheConfig.forEach(function(cacheOption) {
-      var absoluteUrl = new URL(cacheOption[0], baseUrl).toString();
-      var cacheName = cacheNamePrefix + absoluteUrl + '-' + cacheOption[1];
-      currentCacheNamesToAbsoluteUrl[cacheName] = absoluteUrl;
-      absoluteUrlToCacheName[absoluteUrl] = cacheName;
-    });
-
-    return {
-      absoluteUrlToCacheName: absoluteUrlToCacheName,
-      currentCacheNamesToAbsoluteUrl: currentCacheNamesToAbsoluteUrl
-    };
-  };
-
-var stripIgnoredUrlParameters = function (originalUrl, ignoreUrlParametersMatching) {
-    var url = new URL(originalUrl);
-
-    url.search = url.search.slice(1) // Exclude initial '?'
-      .split('&') // Split into an array of 'key=value' strings
-      .map(function(kv) {
-        return kv.split('='); // Split each 'key=value' string into a [key, value] array
-      })
-      .filter(function(kv) {
-        return ignoreUrlParametersMatching.every(function(ignoredRegex) {
-          return !ignoredRegex.test(kv[0]); // Return true iff the key doesn't match any of the regexes.
-        });
-      })
-      .map(function(kv) {
-        return kv.join('='); // Join each [key, value] array into a 'key=value' string
-      })
-      .join('&'); // Join the array of 'key=value' strings into a string with '&' in between each
-
-    return url.toString();
-  };
-
-
-var mappings = populateCurrentCacheNames(PrecacheConfig, CacheNamePrefix, self.location);
-var AbsoluteUrlToCacheName = mappings.absoluteUrlToCacheName;
-var CurrentCacheNamesToAbsoluteUrl = mappings.currentCacheNamesToAbsoluteUrl;
-
-function deleteAllCaches() {
-  return caches.keys().then(function(cacheNames) {
-    return Promise.all(
-      cacheNames.map(function(cacheName) {
-        return caches.delete(cacheName);
-      })
-    );
-  });
-}
-
-self.addEventListener('install', function(event) {
-  var now = Date.now();
-
-  event.waitUntil(
-    caches.keys().then(function(allCacheNames) {
-      return Promise.all(
-        Object.keys(CurrentCacheNamesToAbsoluteUrl).filter(function(cacheName) {
-          return allCacheNames.indexOf(cacheName) === -1;
-        }).map(function(cacheName) {
-          var url = new URL(CurrentCacheNamesToAbsoluteUrl[cacheName]);
-          // Put in a cache-busting parameter to ensure we're caching a fresh response.
-          if (url.search) {
-            url.search += '&';
-          }
-          url.search += 'sw-precache=' + now;
-          var urlWithCacheBusting = url.toString();
-
-          console.log('Adding URL "%s" to cache named "%s"', urlWithCacheBusting, cacheName);
-          return caches.open(cacheName).then(function(cache) {
-            var request = new Request(urlWithCacheBusting, {credentials: 'same-origin'});
-            return fetch(request.clone()).then(function(response) {
-              if (response.ok) {
-                return cache.put(request, response);
-              }
-
-              console.error('Request for %s returned a response with status %d, so not attempting to cache it.',
-                urlWithCacheBusting, response.status);
-              // Get rid of the empty cache if we can't add a successful response to it.
-              return caches.delete(cacheName);
-            });
-          });
-        })
-      ).then(function() {
-        return Promise.all(
-          allCacheNames.filter(function(cacheName) {
-            return cacheName.indexOf(CacheNamePrefix) === 0 &&
-                   !(cacheName in CurrentCacheNamesToAbsoluteUrl);
-          }).map(function(cacheName) {
-            console.log('Deleting out-of-date cache "%s"', cacheName);
-            return caches.delete(cacheName);
-          })
-        );
-      });
-    }).then(function() {
-      if (typeof self.skipWaiting === 'function') {
-        // Force the SW to transition from installing -> active state
-        self.skipWaiting();
-      }
-    })
+  // On install, cache resources and skip waiting so the worker won't
+  // wait for clients to be closed before becoming active.
+  self.addEventListener('install', event =>
+    event.waitUntil(
+      oghliner.cacheResources()
+      .then(() => self.skipWaiting())
+    )
   );
-});
 
-if (self.clients && (typeof self.clients.claim === 'function')) {
-  self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim());
+  // On activation, delete old caches and start controlling the clients
+  // without waiting for them to reload.
+  self.addEventListener('activate', event =>
+    event.waitUntil(
+      oghliner.clearOtherCaches()
+      .then(() => self.clients.claim())
+    )
+  );
+
+  // Retrieves the request following oghliner strategy.
+  self.addEventListener('fetch', event => {
+    if (event.request.method === 'GET') {
+      event.respondWith(oghliner.get(event.request));
+    } else {
+      event.respondWith(self.fetch(event.request));
+    }
   });
-}
 
-self.addEventListener('message', function(event) {
-  if (event.data.command === 'delete_all') {
-    console.log('About to delete all caches...');
-    deleteAllCaches().then(function() {
-      console.log('Caches deleted.');
-      event.ports[0].postMessage({
-        error: null
+  var oghliner = self.oghliner = {
+
+    // This is the unique prefix for all the caches controlled by this worker.
+    CACHE_PREFIX: 'offline-cache:mykmelez/eggtimer:' + (self.registration ? self.registration.scope : '') + ':',
+
+    // This is the unique name for the cache controlled by this version of the worker.
+    get CACHE_NAME() {
+      return this.CACHE_PREFIX + '6c3a2af9222ccb80ca6aa753de848530ce974758';
+    },
+
+    // This is a list of resources that will be cached.
+    RESOURCES: [
+      './index.html', // eaaf8c482e41b792677b2eae6413f8f8b9346670
+      './css/index.css', // 037dd2cfac26017bafd00085121e818eca059393
+      './js/index.js', // 1ebe9a43521b8adf07620174527dee080d99fe3f
+      './js/offline-manager.js', // baf204692e523b9a71f5686f5df47e20ed16a2f7
+      './favicon/android-chrome-144x144.png', // 3b5d96cc1a852daba853e70afa4422fdce66ff13
+      './favicon/android-chrome-192x192.png', // b1b04106f9107e40b26728b285b4e6983b8cd217
+      './favicon/android-chrome-36x36.png', // c10f72cdbd4e241c2ce208d63b97f32b0ddc146e
+      './favicon/android-chrome-48x48.png', // d443b1563b6e8e731619ac4be224d4e6f0f6bcec
+      './favicon/android-chrome-72x72.png', // 9b6f949a972d39b220a5751e4b9bf4f46697e909
+      './favicon/android-chrome-96x96.png', // b543cfd08138ba8824e708322c840a34e834eb14
+      './favicon/apple-touch-icon-114x114.png', // 26d42de151e2011b72be6b7ff745aac64f4dad13
+      './favicon/apple-touch-icon-120x120.png', // 3cb34262ccc3077ca8aaa5d4e263ba9c4dc82045
+      './favicon/apple-touch-icon-144x144.png', // 53fc77d3e353d2facc2ce6e703744b985a87ad94
+      './favicon/apple-touch-icon-152x152.png', // 7c5fb3aed2cbbc5fab91fb0a42d449c6496e5762
+      './favicon/apple-touch-icon-180x180.png', // d9f38d2c03e5d6d3c2cccc3232396b08b2793d34
+      './favicon/apple-touch-icon-57x57.png', // d0072a8374f5e8086274913e732dea8e0f5e020c
+      './favicon/apple-touch-icon-60x60.png', // 9c1d36663ccb092a963325e6ef94a27310af9f08
+      './favicon/apple-touch-icon-72x72.png', // 5b9d842efdd7f9e8967d5f97d9302e435482f62a
+      './favicon/apple-touch-icon-76x76.png', // 8154389698072b351cac6c70e95bfaacb58b2813
+      './favicon/apple-touch-icon-precomposed.png', // 0ea7d8f5c6b4cbe2c4649d7552fa81cc813676d5
+      './favicon/apple-touch-icon.png', // d9f38d2c03e5d6d3c2cccc3232396b08b2793d34
+      './favicon/browserconfig.xml', // 00b510a2fce91ebabc32505ceea8cfe13336950e
+      './favicon/favicon-16x16.png', // 1a0255f4c91e2ef5ea79bdeb95eb4be18cba004f
+      './favicon/favicon-194x194.png', // 835b5b75b2ea63b9352b5b7491085c8857257a67
+      './favicon/favicon-32x32.png', // 66e4e15bb4f659d1fc485f125cc7c85d4a503412
+      './favicon/favicon-96x96.png', // 9b45321ad5ab4b1ee28ef87f1440696cf070fc7f
+      './favicon/favicon.ico', // ef12f58ee036ac192a1e58cdbdcd83dd6376547d
+      './favicon/manifest.json', // 2a2a237c155898b42a3ff46ed298bbe4865a57e5
+      './favicon/mstile-144x144.png', // fc301b91b821c9bec7a69aadecc4801bb3b592df
+      './favicon/mstile-150x150.png', // df60755bea2ebaf57fe4bd9b6e6b086e5ae7b6d9
+      './favicon/mstile-310x150.png', // b52547de5fbc0a9b2aedfbfa87602a6ace1eb84f
+      './favicon/mstile-310x310.png', // 2c296befd44135699244501d1fdd59b2df40f4dc
+      './favicon/mstile-70x70.png', // 353345ccc28c64b47653f21c031acb677278c3f6
+
+    ],
+
+    // Adds the resources to the cache controlled by this worker.
+    cacheResources: function () {
+      var now = Date.now();
+      var baseUrl = self.location;
+      return this.prepareCache()
+      .then(cache => Promise.all(this.RESOURCES.map(resource => {
+        // Bust the request to get a fresh response
+        var url = new URL(resource, baseUrl);
+        var bustParameter = (url.search ? '&' : '') + '__bust=' + now;
+        var bustedUrl = new URL(url.toString());
+        bustedUrl.search += bustParameter;
+
+        // But cache the response for the original request
+        var requestConfig = { credentials: 'same-origin' };
+        var originalRequest = new Request(url.toString(), requestConfig);
+        var bustedRequest = new Request(bustedUrl.toString(), requestConfig);
+        return fetch(bustedRequest)
+        .then(response => {
+          if (response.ok) {
+            return cache.put(originalRequest, response);
+          }
+          console.error('Error fetching ' + url + ', status was ' + response.status);
+        });
+      })));
+    },
+
+    // Remove the offline caches not controlled by this worker.
+    clearOtherCaches: function () {
+      var outOfDate = cacheName => cacheName.startsWith(this.CACHE_PREFIX) && cacheName !== this.CACHE_NAME;
+
+      return self.caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames
+        .filter(outOfDate)
+        .map(cacheName => self.caches.delete(cacheName))
+      ));
+    },
+
+    // Get a response from the current offline cache or from the network.
+    get: function (request) {
+      return this.openCache()
+      .then(cache => cache.match(() => this.extendToIndex(request)))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return self.fetch(request);
       });
-    }).catch(function(error) {
-      console.log('Caches not deleted:', error);
-      event.ports[0].postMessage({
-        error: error
-      });
-    });
-  }
-});
+    },
 
+    // Make requests to directories become requests to index.html
+    extendToIndex: function (request) {
+      var url = new URL(request.url, self.location);
+      var path = url.pathname;
+      if (path[path.length - 1] !== '/') {
+        return request;
+      }
+      url.pathname += 'index.html';
+      return new Request(url.toString(), request);
+    },
 
-self.addEventListener('fetch', function(event) {
-  if (event.request.method === 'GET') {
-    var urlWithoutIgnoredParameters = stripIgnoredUrlParameters(event.request.url,
-      IgnoreUrlParametersMatching);
+    // Prepare the cache for installation, deleting it before if it already exists.
+    prepareCache: function () {
+      return self.caches.delete(this.CACHE_NAME)
+      .then(() => this.openCache());
+    },
 
-    var cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
-    var directoryIndex = 'index.html';
-    if (!cacheName && directoryIndex) {
-      urlWithoutIgnoredParameters = addDirectoryIndex(urlWithoutIgnoredParameters, directoryIndex);
-      cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
+    // Open and cache the offline cache promise to improve the performance when
+    // serving from the offline-cache.
+    openCache: function () {
+      if (!this._cache) {
+        this._cache = self.caches.open(this.CACHE_NAME);
+      }
+      return this._cache;
     }
 
-    var navigateFallback = '';
-    // Ideally, this would check for event.request.mode === 'navigate', but that is not widely
-    // supported yet:
-    // https://code.google.com/p/chromium/issues/detail?id=540967
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1209081
-    if (!cacheName && navigateFallback && event.request.headers.has('accept') &&
-        event.request.headers.get('accept').includes('text/html')) {
-      var navigateFallbackUrl = new URL(navigateFallback, self.location);
-      cacheName = AbsoluteUrlToCacheName[navigateFallbackUrl.toString()];
-    }
-
-    if (cacheName) {
-      event.respondWith(
-        // We can't call cache.match(event.request) since the entry in the cache will contain the
-        // cache-busting parameter. Instead, rely on the fact that each cache should only have one
-        // entry, and return that.
-        caches.open(cacheName).then(function(cache) {
-          return cache.keys().then(function(keys) {
-            return cache.match(keys[0]).then(function(response) {
-              return response || fetch(event.request).catch(function(e) {
-                console.error('Fetch for "%s" failed: %O', urlWithoutIgnoredParameters, e);
-              });
-            });
-          });
-        }).catch(function(e) {
-          console.error('Couldn\'t serve response for "%s" from cache: %O', urlWithoutIgnoredParameters, e);
-          return fetch(event.request);
-        })
-      );
-    }
-  }
-});
-
+  };
+}(self));
